9642a04752b7544f9d7ed80bb6b83baad295520a
matiasandina <matiasandina@gmail.com>
2025-10-31 08:40:52 -0400

    adding dark and light palettes

--- R/palettes.R
+++ R/palettes.R
@@ -7,9 +7,15 @@
   autumn = c(chloro="#27af27", xantho="#f0e57c", caro="#f26d08", antho="#bf2b2b"),
   color_wheel = c(green="#24c22dff", blue="#1f55b5ff", purple="#d63affff",
                   red="#f02e13ff", orange="#feb93aff", yellow="#fff73aff"),
+  dark = c(red = "#d03a22ff", turquoise ="#187376ff", gray = "#727061ff", green = "#257d26ff",
+           purple = "#a039a6ff", cement = "#64676cff", blue = "#1f55b5ff", brown = "#a05917ff",
+           lightblue = "#ccf0ffff"),
   flying_fabric = c(parachute="#f75e45ff", paraglider="#1f55b5ff", hangglider="#ef9a27ff",
                     kitesurf="#ffac8fff", kite="#6dbb4bff", wingsuit="#303030ff"),
   landlocked = c(access="#b0e8a1ff", landlocked="#feaa3aff", doublelocked="#cd4d4dff"),
+  light = c(red = "#fd6041ff", turquoise = "#44b7beff", gray = "#97957eff", green = "#45b046ff",
+            purple = "#cb64d1ff", cement = "#878b94ff", blue = "#5489d5ff", brown = "#f2922cff",
+            lightblue = "#fcf7daff"),
   suit = c(jacket="#7b7c6eff", shirt="#6e88a0ff", accent="#af676aff"),
   surfing = c(wave="#b8d3e0ff", sand="#f0d299ff", rock="#bdb4a3ff", accent="#fd6041ff")
 )
